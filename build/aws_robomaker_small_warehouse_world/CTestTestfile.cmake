34738fc907317184e5baf8401ede0062190ceb1b
boltonathitdavies <athit.jake@gmail.com>
2026-08-17 11:59:18 +0700

    remove build files

--- build/aws_robomaker_small_warehouse_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ambushee/wil_project/aws-robomaker-small-warehouse-world
-# Build directory: /home/ambushee/wil_project/aws-robomaker-small-warehouse-world/build/aws_robomaker_small_warehouse_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
